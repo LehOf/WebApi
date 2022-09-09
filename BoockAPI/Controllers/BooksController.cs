2dbb7d1932f5c0e4fe824fbc535a20e7ecc91555
Leila Contel <leeilacontell@gmail.com>
2022-09-09 17:07:51 -0300

    Add project files.

---
+++ BoockAPI/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BoockAPI.Model;
+using BoockAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoockAPI.Controllers
+{
+    [Route("api/{controller}")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        //CRIANDO ENDPOINTS DA APLICAÇÃO
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Ceate(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+
+            if (bookToDelete == null)
+                return NotFound();
+
+            await _bookRepository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+                return BadRequest();
+
+            await _bookRepository.Update(book);
+            return NoContent();
+        }
+    }
+}
---
+++ BoockAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using BoockAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BoockAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Ceate(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
